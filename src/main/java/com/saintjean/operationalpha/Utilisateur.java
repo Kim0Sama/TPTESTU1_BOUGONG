d4f74a4196e66f70f6cebbfe709ac6a7dc927c51
Pascal0Abega <baape2004@gmail.com>
2025-11-11 21:31:21 +0100

    (FEAT) : Creation de la classe Utilisateur

---
+++ src/main/java/com/saintjean/operationalpha/Utilisateur.java
@@ -0,0 +1,12 @@
+package com.saintjean.operationalpha;
+
+public class Utilisateur {
+    private int id;
+    private String nom;
+    private int age;
+    private String email;
+    private String telephone;
+    private String ville;
+    private double soldePersonnel;
+   
+}
